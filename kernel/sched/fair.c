b94721960636c6546c9327aa34ab7c173969005a
Jesse Chan <jc@lineageos.org>
2018-04-30 16:12:10 -0700

    sched: ehmp: use existing scheduler functions
    Change-Id: I95a02b64d210f864f34d9953dc355cf753ec4233

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5981,7 +5981,7 @@ static int wake_affine(struct sched_domain *sd, struct task_struct *p,
 	return 1;
 }
 
-static inline unsigned long task_util(struct task_struct *p)
+inline unsigned long task_util(struct task_struct *p)
 {
 #ifdef CONFIG_SCHED_WALT
 	if (!walt_disabled && sysctl_sched_use_walt_task_util) {
@@ -6129,7 +6129,7 @@ boosted_task_util(struct task_struct *task)
 	return util + margin;
 }
 
-static int cpu_util_wake(int cpu, struct task_struct *p);
+int cpu_util_wake(int cpu, struct task_struct *p);
 
 /*
  * find_idlest_group finds and returns the least busy CPU group within the
@@ -6547,7 +6547,7 @@ done:
  * cpu_util_wake: Compute cpu utilization with any contributions from
  * the waking task p removed.
  */
-static int cpu_util_wake(int cpu, struct task_struct *p)
+int cpu_util_wake(int cpu, struct task_struct *p)
 {
 	unsigned long util, capacity;
 
